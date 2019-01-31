f0aab1495a47662bb77011f37aa5b0f0fd98e044
Multipixelone <finn@cnwr.net>
2019-01-30 22:05:44 -0800

    Adding gmail to my list

--- start/dome.js
+++ start/dome.js
@@ -36,7 +36,8 @@ var sites = {
 				"School": {
 					"Google Drive" : "https://drive.google.com",
 					"Canvas" : "https://bsd.instructure.com",
-					"Bell Schedule" : "https://www.beaverton.k12.or.us/schools/southridge/calendar/Pages/Bell-Schedule.aspx"
+					"Gmail" : "https://mail.google.com",
+                    "Bell Schedule" : "https://www.beaverton.k12.or.us/schools/southridge/calendar/Pages/Bell-Schedule.aspx"
 				},
 				"Intranet": {
 					"ESXi" : "https://192.168.123.43/ui/#/host",
